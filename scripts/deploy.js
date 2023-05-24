90efa34242125ae28b14d220b1d87d08ce8790c2
RainbowSix(RSIX) <134370409+TokenRSIX@users.noreply.github.com>
2023-05-24 04:32:34 +0000

    1

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+const hre = require("hardhat");
+
+async function main() {
+  const RainbowSix = await hre.ethers.getContractFactory("RainbowSix");
+  const rainbowSix = await RainbowSix.deploy(100000000, 50);
+
+  await rainbowSix.deployed();
+
+  console.log("RainbowSix deployed: ", rainbowSix.address);
+}
+
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
